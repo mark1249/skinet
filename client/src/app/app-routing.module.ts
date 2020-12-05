3cf047bd48a22ae3e47e70d5041ff5181de5094f
mark1249 <mark@brickscreative.com>
2020-12-05 13:41:27 +0000

    End of Section 10v2

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ProductDetailsComponent } from './shop/product-details/product-details.component';
-import { ShopComponent } from './shop/shop.component';
 
 
 const routes: Routes = [
